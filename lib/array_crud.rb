3076e2d3f4a55418fa1d73d68bf8b1eefda9ca42
Aaron Cherniavsky <aaron.b.cherniavsky@gmail.com>
2019-10-13 17:44:16 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["pikachu","Gyrados","Snorlax","Gardevoir"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  << "Sylvion"
 end
 
 def add_element_to_start_of_array(array, element)
